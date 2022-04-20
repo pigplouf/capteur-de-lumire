997d418b81fce649dd5ee6a9e20a73ac9472baff
pigplouf <103496137+pigplouf@users.noreply.github.com>
2022-04-20 10:23:59 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+radio.setGroup(5)
+basic.forever(function () {
+    if (true) {
+    	
+    } else {
+    	
+    }
+    basic.pause(10000)
+})
